7e0b9d74a2db001da1cbd4ce169c8038982d51e0
narramuni <nmspavan@gmail.com>
2023-10-18 18:37:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+        }
+
+        static void doApp()
+        {
+            Student[] std = new Student[4];
+
+
+            for (int i = 0; i < std.Length; i++)
+            {
+
+                std[i].Id = i;
+                Console.WriteLine(std[i].Id + ",");
+
+                std[i].Name = "Name " + i.ToString();
+                Console.Write(std[i].Name + ",");
+
+                std[i].Address = "Address of student ";
+                Console.Write(std[i].Address + ",");
+
+
+                std[i].dataOfBirth = Convert.ToDateTime("2012-12-06 02:26:46");
+                Console.Write(std[i].dataOfBirth);
+
+
+
+
+                Console.ReadLine();
+
+
+            }
+            
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssist
+{
+    public struct Student
+    {
+        public int Id;
+        public string Name;
+        public DateTime dataOfBirth;
+        public string Address;
+      
+
+    }
+
+
+
+}
